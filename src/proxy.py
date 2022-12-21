0da7b67058d26b663aad4f49e9afa30819bd9071
Adam Naoui <adam.naoui@polymtl.ca>
2022-12-21 11:33:59 -0500

    adding ssh tunnels forwarder between each slave to master

--- src/proxy.py
+++ src/proxy.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python
 from flask import Flask
 from pythonping import ping
-
+from sshtunnel import SSHTunnelForwarder
 from src.proxy_constants import MASTER_IP, FIRST_SLAVE_IP, SECOND_SLAVE_IP, THIRD_SLAVE_IP
+
+
+def get_instance_ping(host):
+    ping_result = ping(target=host, timeout=3, count=10)
+    return ping_result.rtt_avg_ms
+
+
+master = {"NAME": "Master node", "IP": MASTER_IP, "PORT": 3306}
+first_slave = {"NAME": "Slave node 1", "IP": FIRST_SLAVE_IP, "PORT": 3307}
+second_slave = {"NAME": "Slave node 2", "IP": SECOND_SLAVE_IP, "PORT": 3308}
+third_slave = {"NAME": "Slave node 3", "IP": THIRD_SLAVE_IP, "PORT": 3309}
+
+slaves = [first_slave, second_slave, third_slave]
+
+# creating ssh tunnels form slaves to master
+for slave in slaves:
+    tunnel = SSHTunnelForwarder(
+        (slave["IP"], 22),
+        ssh_pkey="/home/ubuntu/private_key_PROJET_KEY.pem",
+        ssh_username="ubuntu",
+        allow_agent=False,
+        local_bind_address=('127.0.0.1', slave["PORT"]),
+        remote_bind_address=(master["IP"], master["PORT"]))
+    tunnel.start()
 
 app = Flask(__name__)
 
@@ -20,16 +44,3 @@ def custom_endpoint():
 @app.route('/random')
 def random_endpoint():
     pass
-
-
-def get_instance_ping(host):
-    ping_result = ping(target=host, timeout=3, count=10)
-    return ping_result.rtt_avg_ms
-
-
-master = {"name": "Master node", "ip": MASTER_IP, "port": 3306}
-first_slave = {"name": "Slave node 1", "ip": FIRST_SLAVE_IP, "port": 3307}
-second_slave = {"name": "Slave node 2", "ip": SECOND_SLAVE_IP, "port": 3308}
-third_slave = {"name": "Slave node 3", "ip": THIRD_SLAVE_IP, "port": 3309}
-
-
